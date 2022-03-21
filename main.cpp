7bc9cc43d900e0e842effe8b2b7e2a310cd7fbdb
Svartov <100573588+Svartov@users.noreply.github.com>
2022-03-21 20:49:23 +0000

    kalkulator

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
-using namespace std; 
+#include <stdio.h>
 
-int main() {
-  cout << "Hello World!\n";
-
-  return 0;
+int main()
+{
+    double a, b, c;
+    char d;
+    int p = 1;
+    while (p != 2)
+    {
+        printf("1 - calculator\n2 - exit\n");
+        scanf("%d", &p);
+        if (p != 1) break;
+        printf("Write expression (without spaces): ");
+        scanf("%lf%c%lf", &a,&d,&b);
+        while(getchar() != '\n');
+        switch(d)
+        {
+        case '+': c = a + b; break;
+        case '-': c = a - b; break;
+        case '*': c = a * b; break;
+        case '/': c = a / b; break;
+        default:
+            printf("Wrong action\n");
+            continue;
+        }
+        printf("Result = %lf\n", c);
+    }
 }
